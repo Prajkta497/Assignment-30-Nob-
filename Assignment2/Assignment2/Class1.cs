3dfb45b264d5668ca39a6a5973905d82797abd02
Prajkta497 <75295907+Prajkta497@users.noreply.github.com>
2020-12-01 11:51:29 +0530

    Add files via upload

---
+++ Assignment2/Assignment2/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    //2. Can we have protected abstract methods in abstract class.
+    //Answer := YES
+   public abstract class Class1
+    {
+        protected abstract void MyMethod();
+      
+    }
+}
